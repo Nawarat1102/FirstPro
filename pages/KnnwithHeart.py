3608d434f145ef239a4756f5700b592aa0f67cf2
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2025-08-28 18:49:04 +0700

    .

--- pages/KnnwithHeart.py
+++ pages/KnnwithHeart.py
@@ -6,6 +6,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from streamlit_lottie import st_lottie
 import requests
+import time
+
 
 # ====== ฟังก์ชันโหลด Lottie ======
 def load_lottieurl(url: str):
@@ -20,8 +22,11 @@ from streamlit_lottie import st_lottie
 with open("./img/Trackingofassets.json") as f:
     lottie_success = json.load(f)
 
-with open("./img/Trackingofassets.json") as f:
+with open("./img/AddFile.json") as f:
     lottie_failure = json.load(f)
+
+with open("./img/Safety.json") as f:
+    lottie_loading = json.load(f)
 
 
 st.title('การทำนายข้อมูลโรคเบาหวานระยะเริ่มต้น K-Nearest Neighbor')
@@ -100,6 +105,10 @@ A15 = st.number_input("ผมร่วง (ใช่=1 ไม่ใช่=0)")
 A16 = st.number_input("โรคอ้วน (ใช่=1 ไม่ใช่=0)")
 
 if st.button("ทำนายผล"):
+   
+   with st.spinner('กำลังประมวลผล...'):
+        st_lottie(lottie_loading, height=150)  # แสดง animation loading
+        time.sleep(4)  # จำลองเวลาโหลด
    #st.write("ทำนาย")
    #dt = pd.read_csv("./data/iris-3.csv") 
    X = dt.drop('class', axis=1)
