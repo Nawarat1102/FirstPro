eb7abf3a01828a7495fd82d84a6883e3b341ae15
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2025-08-28 16:46:29 +0700

    .

--- pages/KnnwithHeart.py
+++ pages/KnnwithHeart.py
@@ -15,8 +15,8 @@ def load_lottieurl(url: str):
     return r.json()
 
 # URL ของ Lottie animations
-lottie_url_success = "https://assets7.lottiefiles.com/packages/lf20_5dd64c83-bab9-4b2b-b289-fd13b1c1fd1c.json"  # ตัวอย่าง animation ผ่าน
-lottie_url_failure = "https://assets7.lottiefiles.com/packages/lf20_5dd64c83-bab9-4b2b-b289-fd13b1c1fd1c.json"   # ตัวอย่าง animation ไม่ผ่าน
+lottie_url_success = "Payment Successful.json"  # ตัวอย่าง animation ผ่าน
+lottie_url_failure = "Payment Successful.json"   # ตัวอย่าง animation ไม่ผ่าน
 
 lottie_success = load_lottieurl(lottie_url_success)
 lottie_failure = load_lottieurl(lottie_url_failure)
